9fa7d208164fc0a1e7e7a04e1a2375a39b85efa5
RUSya82 <mail-spam2012@yandex.ru>
2019-10-10 21:14:15 +0500

    Added comments

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
 <?php
-
-$dir_big = __DIR__ . '/img/big';
+//сканируем директорию с маленькими картинками
 $images_tmp = scandir(__DIR__ . '/img/small');//читаем директорию
-
 $images = [];
 foreach ($images_tmp as $value){
     if(substr($value, -3, 3) === 'jpg')//оставляем только картинки
         $images[] = $value;//составляем массив названий картинок
 }
-
+//подключаем автозагрузчик
 include __DIR__ . '/vendor/autoload.php';
 try{
     $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
     $twig = new \Twig\Environment($loader);
-    echo $twig->render('template.twig', ['images' => $images]);
+    echo $twig->render('template.twig', ['images' => $images]);//передаём в шаблонизатор массив с картинками
 
 } catch (Exception $e){
     die ('ERROR: ' . $e->getMessage());
