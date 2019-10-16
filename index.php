6caab70184fae84969db26775f7baeb253ba9c3c
RUSya82 <mail-spam2012@yandex.ru>
2019-10-16 16:32:58 +0500

    final lesson5

--- index.php
+++ index.php
@@ -1,18 +1,1 @@
-<?php
-//сканируем директорию с маленькими картинками
-$images_tmp = scandir(__DIR__ . '/img/small');//читаем директорию
-$images = [];
-foreach ($images_tmp as $value){
-    if(substr($value, -3, 3) === 'jpg')//оставляем только картинки
-        $images[] = $value;//составляем массив названий картинок
-}
-//подключаем автозагрузчик
-include __DIR__ . '/vendor/autoload.php';
-try{
-    $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
-    $twig = new \Twig\Environment($loader);
-    echo $twig->render('template.twig', ['images' => $images]);//передаём в шаблонизатор массив с картинками
-
-} catch (Exception $e){
-    die ('ERROR: ' . $e->getMessage());
-}
+<?phpsession_start();function __autoload($classname){	include_once("c/$classname.php");}//site.ru/index.php?act=auth&c=User$action = 'action_';$action .= (isset($_GET['act'])) ? $_GET['act'] : 'index';switch ($_GET['c']){	case 'articles':		$controller = new C_Page();	case 'users':		$controller = new C_User();		break;	default:		$controller = new C_Page();}$controller->Request($action);
